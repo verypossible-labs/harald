7e39b81e4be54e5b508da38e24942c22828d1f5f
Allie Sargente <Allie@verypossible.com>
2020-12-07 14:41:04 -0500

    ATT_PREPARE_WRITE RSP and REQ
    - Implement the ATT_PREPARE_WRITE_REQ PDU
    - Implement the ATT_PREPARE_WRITE_RSP PDU
    - Update elixir_make, pervious version is deprecated
    - Implement the tests for ATT_PREPARE_WRITE PDU's

---
+++ test/harald/host/att/prepare_write_rsp_test.exs
@@ -0,0 +1,48 @@
+defmodule Harald.Host.ATT.PrepareWriteRspTest do
+  use ExUnit.Case, async: true
+  alias Harald.Host.ATT.PrepareWriteRsp
+
+  test "encode/1" do
+    attribute_handle = 185
+    value_offset = 5
+    part_attribute_value = <<72, 101, 108, 108, 111>>
+
+    parameters = %{
+      attribute_handle: attribute_handle,
+      value_offset: value_offset,
+      part_attribute_value: part_attribute_value
+    }
+
+    expected_bin =
+      <<attribute_handle::little-size(16), value_offset::little-size(16),
+        part_attribute_value::binary>>
+
+    expected_size = byte_size(expected_bin)
+
+    assert {:ok, actual_bin} = PrepareWriteRsp.encode(parameters)
+    assert expected_bin == actual_bin
+    assert expected_size == byte_size(actual_bin)
+  end
+
+  test "decode/1" do
+    attribute_handle = 185
+    value_offset = 5
+    part_attribute_value = <<72, 101, 108, 108, 111>>
+
+    bin =
+      <<attribute_handle::little-size(16), value_offset::little-size(16),
+        part_attribute_value::binary>>
+
+    expected_parameters = %{
+      attribute_handle: attribute_handle,
+      value_offset: value_offset,
+      part_attribute_value: part_attribute_value
+    }
+
+    assert {:ok, expected_parameters} == PrepareWriteRsp.decode(bin)
+  end
+
+  test "opcode/0" do
+    assert 0x17 == PrepareWriteRsp.opcode()
+  end
+end
